d2fb5fc6585dd4502f81f9fcdf5e166f9c69fb8b
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-05 11:48:38 +0530

    exception logic addded

--- dashboard/src/main/java/com/finance/dashboard/exception/CustomException.java
+++ dashboard/src/main/java/com/finance/dashboard/exception/CustomException.java
@@ -1,7 +1,8 @@
 package com.finance.dashboard.exception;
 
 public class CustomException extends RuntimeException {
-    public CustomException(String message) {
+
+    public CustomException(String message){
         super(message);
     }
 }
