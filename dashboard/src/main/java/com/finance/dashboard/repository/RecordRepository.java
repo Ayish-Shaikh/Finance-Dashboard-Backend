6b9e386d75a06b989b12d294f68aba388aadea96
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-05 11:49:32 +0530

    repository logic addded

--- dashboard/src/main/java/com/finance/dashboard/repository/RecordRepository.java
+++ dashboard/src/main/java/com/finance/dashboard/repository/RecordRepository.java
@@ -1,4 +1,12 @@
 package com.finance.dashboard.repository;
 
-public interface RecordRepository {
+import com.finance.dashboard.model.FinancialRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecordRepository extends JpaRepository<FinancialRecord , Long> {
+    List<FinancialRecord> findByType(String type);
+    List<FinancialRecord> findByCategory(String category);
+    List<FinancialRecord> findByTypeAndCategory(String type, String category);
 }
--- dashboard/src/main/java/com/finance/dashboard/repository/UserRepository.java
+++ dashboard/src/main/java/com/finance/dashboard/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.finance.dashboard.repository;
 
-public interface UserRepository {
+import com.finance.dashboard.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User , Long> {
+    boolean existsByEmail(@NotBlank(message = "Email cannot be blank") @Email(message = "Invalid Email Format") String email);
+
+    Optional<User> findByEmail(String email);
 }
